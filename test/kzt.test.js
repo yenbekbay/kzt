86a5b10591809011e11027c393d4fb227db19f33
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-02-05 21:29:14 -0600

    Upgrade to ES6

--- test/kzt.test.js
+++ test/kzt.test.js
@@ -1,17 +1,32 @@
-var expect = require('chai').expect;
-var scraper = require('../lib/scraper');
+'use strict';
 
-describe('KKB Scraper', function() {
-  it('retrives rates from the page', function(done) {
-    scraper.getRates().subscribe(function(rates) {
+const expect = require('chai').expect;
+
+const Scraper = require('../lib/scraper');
+
+let scraper;
+
+describe('KKB Scraper', () => {
+  before(() => {
+    let logger = {};
+    logger.warning = function() {};
+    logger.error = function() {};
+
+    scraper = new Scraper(logger);
+  });
+
+  it('retrives rates from the page', done => {
+    scraper.getRates().subscribe(rates => {
       expect(rates).to.be.an('array');
       expect(rates).to.not.be.empty;
-      for (var i = 0; i < rates.length; i++) {
-        var rate = rates[i];
+      for (let i = 0; i < rates.length; i++) {
+        const rate = rates[i];
+
         expect(rate).to.have.property('currency').that.is.a('string');
         expect(rate).to.have.property('buy').that.is.a('number');
         expect(rate).to.have.property('sell').that.is.a('number');
       }
+
       done();
     }, done);
   });
