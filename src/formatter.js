ff8f0c855cefd653f723493ea3432115006a0f1f
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-04-26 19:16:23 -0500

    Move table formatting methods to a separate class

---
+++ src/formatter.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const Table = require('cli-table2');
+
+class Formatter {
+  constructor(rates, currencies) {
+    this.rates = rates;
+    this.currencies = currencies;
+  }
+
+  tablesForAllSources() {
+    return this.rates.map(source => {
+      const colSpan = source.rates
+        .map(rate => rate.sell)
+        .filter(price => !!price).length > 0 ? 3 : 2;
+      const table = new Table({
+        head: [{ hAlign: 'center', colSpan: colSpan, content: source.title }]
+      });
+
+      table.push(colSpan === 2
+        ? ['Currency', 'Weighted']
+        : ['Currency', 'Buy', 'Sell']
+      );
+
+      source.rates
+        .filter(rate => this.currencies.indexOf(rate.currency) > -1)
+        .map(this.constructor._rowFromRate)
+        .forEach(row => table.push(row));
+
+      return table;
+    });
+  }
+
+  tableForAverageRates() {
+    const flatRates = this.rates
+      .map(source => source.rates)
+      .reduce((a, b) => a.concat(b));
+
+    const averageRates = this.currencies.map(currency => {
+      const currencyRates = flatRates
+        .filter(rate => rate.currency === currency);
+
+      return ['buy', 'sell', 'weighted'].reduce((averageRates, key) => {
+        averageRates[key] = this.constructor._averageRate(currencyRates, key);
+        return averageRates;
+      }, { currency: currency });
+    });
+
+    const table = new Table({
+      head: ['Currency', 'Buy', 'Sell', 'Weighted']
+    });
+    averageRates
+      .map(this.constructor._rowFromRate)
+      .forEach(row => table.push(row));
+
+    return table;
+  }
+
+  static _rowFromRate(rate) {
+    return [
+      rate.currency,
+      rate.buy,
+      rate.sell,
+      rate.weighted
+    ].filter(column => !!column);
+  }
+
+  static _averageRate(rates, key) {
+    const totalRates = rates
+      .map(rate => rate[key])
+      .filter(price => !!price);
+    const averageRate = totalRates
+      .reduce((a, b) => a + b) / totalRates.length;
+
+    return Math.round(averageRate * 10000) / 10000;
+  }
+}
+
+module.exports = Formatter;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 'use strict';
 
-const Table = require('cli-table2');
 const updateNotifier = require('update-notifier');
 
+const Formatter = require('./formatter');
 const Logger = require('./logger');
-const Scraper = require('./scraper');
 const pkg = require('../package.json');
+const Scraper = require('./scraper');
 
 const argv = require('yargs')
   .usage('kzt [-h] [-c {"USD", "EUR", "RUB"}] [-a]')
@@ -42,58 +42,14 @@ scraper
     logger.stopSpinner();
     console.log('Exchange rates for KZT (tenge):');
 
+    const formatter = new Formatter(rates, currencies);
+
     if (displayAllSources) {
-      rates.forEach(source => {
-        const colSpan = source.rates
-          .map(rate => rate.sell)
-          .filter(price => !!price).length > 0 ? 3 : 2;
-        const table = new Table({
-          head: [{ hAlign: 'center', colSpan: colSpan, content: source.title }]
-        });
-        table.push(colSpan === 2
-          ? ['Currency', 'Weighted']
-          : ['Currency', 'Buy', 'Sell']
-        );
-
-        source.rates
-          .filter(rate => currencies.indexOf(rate.currency) > -1)
-          .forEach(rate => {
-            table.push([rate.currency, rate.buy, rate.sell, rate.weighted]
-              .filter(column => !!column)
-            );
-          });
-
-        console.log(table.toString());
-      });
+      formatter
+        .tablesForAllSources()
+        .forEach(table => console.log(table.toString()));
     } else {
-      const flatRates = rates
-        .map(source => source.rates)
-        .reduce((a, b) => a.concat(b));
-
-      const averageRates = currencies.map(currency => {
-        const currencyRates = flatRates
-          .filter(rate => rate.currency === currency);
-        const keys = ['buy', 'sell', 'weighted'];
-        let averageRates = { currency: currency };
-        keys.forEach(key => {
-          const totalRates = currencyRates
-            .map(rate => rate[key])
-            .filter(price => !!price);
-          const averageRate = totalRates
-            .reduce((a, b) => a + b) / totalRates.length;
-          averageRates[key] = Math.round(averageRate * 10000) / 10000;
-        });
-        return averageRates;
-      });
-
-      const table = new Table({
-        head: ['Currency', 'Buy', 'Sell', 'Weighted']
-      });
-      averageRates.forEach(rate => {
-        table.push([rate.currency, rate.buy, rate.sell, rate.weighted]);
-      });
-
-      console.log(table.toString());
+      console.log(formatter.tableForAverageRates().toString());
     }
 
     process.exit();
